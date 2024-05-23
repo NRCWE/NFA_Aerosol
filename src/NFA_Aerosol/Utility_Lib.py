e29217513e438342d8d5ab90e56821921cd479c2
Anders Brostrøm <80405973+AndersBros@users.noreply.github.com>
2024-05-23 15:53:02 +0200

    Update Utility_Lib.py

--- src/NFA_Aerosol/Utility_Lib.py
+++ src/NFA_Aerosol/Utility_Lib.py
@@ -368,6 +368,118 @@ def Lung_Dep_ICRP(data_in,bin_mids,respvol,exposure_time=0,plot=0):
 ###############################################################################
 ###############################################################################
 
+def MPS_Ceff(Pdiam,flowrate=0.6):
+    """
+    Function to estimate the collection efficiency of the mini particle sampler (MPS)
+    as a function of particle size and flowrate. It should be noted, that the 
+    expression is only valid up untill a particle diamter of 1 um, from where 
+    a collection efficiency of 1 can be used. 
+    
+    The function assumes that 1.2/1.3 Quantfoil grids were used when sampling.
+    
+    Parameters
+    ----------
+    Pdiam : np.array()
+        Array of particle diameters in um from which to determine the collection
+        efficiency.
+    flowrate : float, optional
+        Flowrate used during sampling in lpm. The default is 0.6 lpm.
+
+    Returns
+    -------
+    E_total : np.array
+        Overall collection efficiency of the MPS at the given size and flow.
+    E_impaction : np.array
+        Collection efficiency from impaction alone at the given size and flow.
+    E_diffusion : np.array
+        Collection efficiency from diffusion to a flat surface alone at the given size and flow.
+    E_interception : np.array
+        Collection efficiency from interception alone at the given size and flow.
+    E_edge : np.array
+        Collection efficiency from diffusion to pore edges alone at the given size and flow.
+
+    """
+    ###########################################################################
+    "Constants and conversions , units        , Descriptions" 
+    rP = (Pdiam/2) * 1e-6      # m            ; Particle radius
+    rhop = 1000                # kg/m3        ; Particle density
+    lambda1 = 0.069 * 1e-6     # m            ; Molecule mean free path
+    Lf = 0.02 * 1e-6           # m            ; Filter thickness
+    N0 = 16003913722018.367    # #            ; Number of pores per surface unit of the carbon film
+    r0 = 0.6 * 1e-6            # m            ; Pore radius
+    A0 = np.pi*(r0**2)         # m2           ; Area of a single pore
+    P  = A0*N0/100             # unitless     ; Porosity
+    rc = r0/np.sqrt(P)         # m            ; Cylindrical aerosol stream passed through a unitary pore
+    df = 2 * 1e-3              # m            ; TEM grid diameter
+    Q  = flowrate/60 * 1e-3    # m3/s         ; Flowrate through the MPS
+    U0 = Q / ((np.pi/4)*df**2) # m/s          ; Flow velocity
+    nu = 1.85e-5               # kg/(m*s)     ; Fluid dynamic viscosity
+    Kb = 1.381e-23             # kg*m2/(s2*K) ; Boltzmann constant
+    T  = 300                   # K            ; Temperature
+    Kn = lambda1 / rP          # unitless     ; Knudsen number
+    Cc = 1 + Kn *(1.165 + 0.483*
+         np.exp(-0.997/Kn))    # unitless     ; Cunningham slipfactor correction
+    D = (Kb*T*Cc)/(6*np.pi*
+                   nu*rP)      # m2/s         ; Diffusion coefficient    
+    
+    ############################## Interception ###############################
+    # Calculate collection efficiency resulting from interception of particles
+    lg = 1.126 * lambda1       # m            ; slip length
+    Ng = lg/r0                 # unitless     ; Slip parameter
+    NG = Ng*(1+Ng/2)           # unitless     ; Parameter
+    Nr = rP/r0                 # unitless     ; Particle radius normalized for pore radius
+    NR = Nr * (1-Nr/2)         # unitless     ; Parameter
+    
+    # Collection efficiency via interception
+    E_interception = ((4*NR**2)/(1+4*NG))*(1+2*(NG/NR)) 
+    
+    ####################### Diffusion to pore edge ############################
+    # Calculate collection efficiency resulting from particles diffusing to the pore edges
+    Pe = U0 / (np.pi*Lf * D * 
+               N0)             # unitless     ; Peclets number
+    
+    # Collection efficiency from pore edges calculated for different flow regimes
+    E_edge = np.ones_like(Pe)
+    E_edge[Pe > 25] = 2.56*Pe[Pe > 25]**(-2/3) -1.2*Pe[Pe > 25]**(-1)-0.177*Pe[Pe > 25]**(-4/3)
+    E_edge[Pe <= 25] =  (1 - 0.81904*np.exp(-3.6568*Pe[Pe <= 25]**(-1)) - 0.09752*
+                np.exp(-22.3045*Pe[Pe <= 25]**(-1))-0.03248*np.exp(-56.95*Pe[Pe <= 25]**(-1)) -
+                0.0157*np.exp(-107.6*Pe[Pe <= 25]**(-1)))
+    
+    ######################### Diffusion to surface ############################
+    # Calculate collection efficiency resulting from particles diffusing to flat filter surfaces
+    alpha2 = 4.5                # unitless    ; Parameter
+    Ds = D/ (rc*U0)             # unitless    ; Normalized diffusion coefficient
+    alpha1 = (4.57-6.46*P
+              +4.58*P**2)       # unitless    ; Parameter
+    
+    # Collection efficiency of filter via diffusion
+    E_diffusion = 1-np.exp(-(alpha1*(Ds**(2/3)))/(1+(alpha1/alpha2)*Ds**(7/15))) 
+
+    ############################## Impaction ##################################
+    # Calculate collection efficiency resulting from impaction
+    stk = (2*Cc*U0*rP**2*rhop
+          )/(9*nu*r0)           # unitless     ; Stokes number
+    eta = np.sqrt(P)/(1-
+          np.sqrt(P))           # unitless     ; Parameter
+    ei = (2*stk *np.sqrt(eta)+ 
+          2 * (stk**2)*eta * 
+          np.exp(-1/(stk*
+          np.sqrt(eta)))-2*
+          (stk**2)*eta)         # unitless     ; Parameter
+    
+    # Collection efficiency via impaction
+    E_impaction = (2 *ei) / (1+eta) - (ei / (1 + eta))**2
+    
+    ############################## Overall Ceff ###############################
+    # Calculate overall collection efficiency of the MPS
+    E_total = 1 - (1-E_impaction)*(1-E_diffusion)*(1-E_interception)*(1-E_edge)
+    
+    return E_total, E_impaction, E_diffusion, E_interception, E_edge
+
+###############################################################################
+###############################################################################
+###############################################################################
+
 def Normalize_dndlogdp(data_in,bin_edges):
     """
     Function to normalize an array of particle xxx concentrations (xxx can be
@@ -559,6 +671,51 @@ def num2vol(data_in,bins_in):
     Data_return[:,1] = total_vol
     
     return Data_return
+    
+###############################################################################
+###############################################################################
+###############################################################################
+
+def Partector_Ceff(Psize):
+    """
+    Function to estimate the collection efficiency of the partectorTEM at the
+    specified particle size in nm. The collection efficiency as a fraction is 
+    returned and can be multiplied with the measured concentration to get a 
+    corrected concentration.
+    
+    It should be noted, that the expression for the collection efficiency has
+    only been validated for NaCl and Ag particles in the size range from 3 to
+    320 nm, and may therefore not be accurate at um sizes!
+    
+    Reference: Fierz, M., Kaegi, R., and Burtscher, H.;"Theoretical and 
+    Experimental Evaluation of a Portable Electrostatic TEM Sampler", Aerosol
+    Science and Technology, 41, issue 5, 2007.
+
+    Parameters
+    ----------
+    Psize : float or np.array
+        Either a single particle size given as a float, or an array of particle
+        sizes to be used for calculating the collection efficiency.
+
+    Returns
+    -------
+    Collection_efficiency : float or np.array
+        The calculated collection efficiency of the PartectorTEM at the specified
+        particle size/sizes specified as a fraction (0-1).
+
+    """
+    Collection_efficiency = (1700*Psize**(-1.78))/100
+    
+    def is_array(param):
+        return type(param) is np.ndarray
+
+    if is_array(Psize):
+        Collection_efficiency[Psize <= 10] = 1
+    else:
+        if Psize <= 10:
+            Collection_efficiency = 1
+            
+    return Collection_efficiency 
     
 ###############################################################################
 ###############################################################################
@@ -1024,26 +1181,3 @@ def calculate_average(data, start_time, end_time):
         return np.mean(filtered_data[:, 1])
     else:
         return np.nan  # Return NaN if no data is available in the time span
-
-def filter_data(data, start_time, end_time):
-    """
-    Function to filter data for a specific time span
-
-    Parameters
-    ----------
-    data : numpy.array
-        A 2D array where the first column contains datetime objects and the 
-        second column contains numerical values for which the average is to be calculated.
-    start_time : datetime
-        The start time for the period over which to calculate the average.
-    end_time : datetime
-        The end time for the period over which to calculate the average.
-
-    Returns
-    -------
-    data : numpy.array
-        
-        Created by: PLF
-    """
-    filtered_data = data[(data[:, 0] >= start_time) & (data[:, 0] <= end_time)]
-    return filtered_data
